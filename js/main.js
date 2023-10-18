481ed638d5dba7a11569b192cfc54218ef2c2db4
ohayonb <ohayonzzb@gmail.com>
2023-10-18 14:34:33 +0300

    generate api call using fetch, and log the results copied ad card from the link given using devtools added taboola favicon

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict'
+
+async function getAds() {
+  try {
+    const res = await fetch(
+      'http://api.taboola.com/1.0/json/taboola-templates/recommendations.get?publisher id=taboola-templates&app.type=desktop&app.apikey=f9040ab1b9c802857aa783c469d0e0ff7e7366e4&source.id=141411111&count=10&source.type=photo',
+      {
+        method: 'GET',
+      }
+    )
+    const data = await res.json()
+    console.log(data)
+    return data.list
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+async function renderAds() {
+  const ads = await getAds()
+  console.log(ads)
+}
+
+renderAds()
